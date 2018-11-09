ae9199c721f16452979dcbf3d83890b6b6aed1c8
AIsacson <aisacson@kth.se>
2018-11-09 18:25:21 +0100

    Star model added
    Star model added instead of standard cube. Background music added,
    needs trimming more.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Transform[] cubesSpawnPoints;
+	public Transform[] nextStageSpawnPoints;
+	public GameObject cube;
+	public Text scoreBoard;
+
+	private int score;
+
+	// Use this for initialization
+	void Awake () {
+		SpawnFirstCubes ();
+		scoreBoard.text = "Score: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnFirstCubes() {
+		for (int i = 0; i < cubesSpawnPoints.Length; i++) {
+			cube = Instantiate (cube, cubesSpawnPoints [i].position, cubesSpawnPoints [i].rotation);
+		}
+		for (int i = 0; i < nextStageSpawnPoints.Length; i++) {
+			cube = Instantiate (cube, nextStageSpawnPoints [i].position, nextStageSpawnPoints [i].rotation);
+		}
+	}
+
+	/*public void StartNextStage() {
+
+	}*/
+
+	public void AddScore(int newScore) {
+		score += newScore+1;
+		UpdateScore ();
+
+	}
+
+	void UpdateScore() {
+		scoreBoard.text = "Score: " + score;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float speed;
+
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector3 movement;
+	private Controller controller;
+	private GameController gameController;
+	private int score;
+
+	// Use this for initialization
+	void Awake() {
+		rb = GetComponent<Rigidbody> ();
+		score = 0;
+		gameController = FindObjectOfType<GameController> ();
+	}
+
+	void Start() {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveVertical = Input.GetAxis ("Vertical");
+		moveHorizontal = Input.GetAxis ("Horizontal");
+
+		movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Cube") {
+			other.gameObject.SetActive (false);
+			gameController.AddScore (score);
+		}
+		if (other.tag == "Flag") {
+			//gameController.StartNextStage ();
+			Destroy (other.gameObject);
+		}
+	}
+}
